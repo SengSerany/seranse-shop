72227cfa6f6517198a1dc8c99f03d2c04604d090
SengSerany <cranyseng@gmail.com>
2022-07-08 22:23:21 +0200

    create basic routes for login, register and index product + header components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/layout/Navigation';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+import IndexProducts from './pages/products/IndexProducts';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Navigation />
+        <div className="container">
+          <Routes>
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/" element={<IndexProducts />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
